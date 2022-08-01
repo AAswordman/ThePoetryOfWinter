1d81b55c5d522d964abcd85130a8589314bf1a0a
AAswordman <1002153674@qq.com>
2022-08-01 20:17:38 +0800

    new file:   ExuiKrnln.dll 	new file:   FileDriver.sys 	new file:   config.txt 	modified:   wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js 	modified:   wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js.map 	deleted:    wp/WPB2/scripts/src/main/TpTo.js 	modified:   wp/WPB2/scripts/src/modules/exmc/ExServerEvents.ts
    Former-commit-id: c1da8a97706fc971ce4168849d84d4509810a103

--- wp/WPB2/scripts/src/modules/exmc/ExServerEvents.ts
+++ wp/WPB2/scripts/src/modules/exmc/ExServerEvents.ts
@@ -1,4 +1,4 @@
-import { world, Events } from "mojang-minecraft";
+import { world, Events, TickEvent } from 'mojang-minecraft';
 import ExEventManager from "./interface/ExEventManager.js";
 import ExGameServer from "./ExGameServer.js";
 import ExGameConfig from './ExGameConfig.js';
@@ -7,7 +7,42 @@ import ExErrorStack from './ExErrorStack.js';
 export default class ExServerEvents implements ExEventManager {
 	public events: Events;
 	private _server: ExGameServer;
-	public exEvents = {};
+	public exEvents = {
+		"onLongTick": {
+			subscribe: (callback: (arg: TickEvent) => void) => {
+				this._subscribe("tick", callback);
+			},
+			unsubscribe: (callback: (arg: TickEvent) => void) => {
+				this._unsubscribe("tick", callback)
+			},
+			pattern: () => {
+				this.events.tick.subscribe((e) => {
+					this.tickNum++;
+					this.tickTime+=e.deltaTime
+					if(this.tickNum >= 5){
+
+					}
+				});
+			}
+		}
+
+	};
+	static monitorMap = new Map<string, ((arg: any) => void)[]>;
+	tickNum: number = 0;
+	tickTime: number = 0;
+	_subscribe(name: string, callback: (arg: any) => void) {
+		let e = ExServerEvents.monitorMap.get(name) ?? [];
+		e.push(callback);
+
+	}
+
+	_unsubscribe(name: string, callback: (arg: any) => void) {
+		let arr = ExServerEvents.monitorMap.get(name) ?? [];
+
+		arr.splice(arr.findIndex((v, i) => {
+			if (v === callback) return true;
+		}), 1);
+	}
 
 	constructor(server: ExGameServer) {
 		this._server = server;
