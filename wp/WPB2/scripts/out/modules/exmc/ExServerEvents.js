c1da8a97706fc971ce4168849d84d4509810a103
AAswordman <1002153674@qq.com>
2022-08-01 20:17:38 +0800

    new file:   ExuiKrnln.dll 	new file:   FileDriver.sys 	new file:   config.txt 	modified:   wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js 	modified:   wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js.map 	deleted:    wp/WPB2/scripts/src/main/TpTo.js 	modified:   wp/WPB2/scripts/src/modules/exmc/ExServerEvents.ts

--- wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js
+++ wp/WPB2/scripts/out/modules/exmc/ExServerEvents.js
@@ -1,9 +1,41 @@
-import { world } from "mojang-minecraft";
+import { world } from 'mojang-minecraft';
 export default class ExServerEvents {
     constructor(server) {
-        this.exEvents = {};
+        this.exEvents = {
+            "onLongTick": {
+                subscribe: (callback) => {
+                    this._subscribe("tick", callback);
+                },
+                unsubscribe: (callback) => {
+                    this._unsubscribe("tick", callback);
+                },
+                pattern: () => {
+                    this.events.tick.subscribe((e) => {
+                        this.tickNum++;
+                        this.tickTime += e.deltaTime;
+                        if (this.tickNum >= 5) {
+                        }
+                    });
+                }
+            }
+        };
+        this.tickNum = 0;
+        this.tickTime = 0;
         this._server = server;
         this.events = world.events;
+    }
+    _subscribe(name, callback) {
+        var _a;
+        let e = (_a = ExServerEvents.monitorMap.get(name)) !== null && _a !== void 0 ? _a : [];
+        e.push(callback);
+    }
+    _unsubscribe(name, callback) {
+        var _a;
+        let arr = (_a = ExServerEvents.monitorMap.get(name)) !== null && _a !== void 0 ? _a : [];
+        arr.splice(arr.findIndex((v, i) => {
+            if (v === callback)
+                return true;
+        }), 1);
     }
     register(name, fun) {
         let func = fun;
@@ -24,4 +56,5 @@ export default class ExServerEvents {
         }
     }
 }
+ExServerEvents.monitorMap = new Map;
 //# sourceMappingURL=ExServerEvents.js.map
