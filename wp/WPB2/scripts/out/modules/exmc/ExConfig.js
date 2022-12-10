03d2b92c0f18418d36563a285da95c27f635cb55
AAswordman <1002153674@qq.com>
2022-12-10 21:18:21 +0800

    1

--- wp/WPB2/scripts/out/modules/exmc/ExConfig.js
+++ wp/WPB2/scripts/out/modules/exmc/ExConfig.js
@@ -1,7 +1,7 @@
 export default class ExConfig {
     constructor() {
         this.gameVersion = "1.9.20";
-        this.addonVersion = "1.6.41";
+        this.addonVersion = "1.6.42";
         this.debug = false;
         this.watchDog = false;
     }
