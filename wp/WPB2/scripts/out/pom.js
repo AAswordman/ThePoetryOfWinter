ca01ff42cda66956e41dec7a7d8821f77e12b85c
AAswordman <1002153674@qq.com>
2023-01-27 21:47:32 +0800

    NEW

--- wp/WPB2/scripts/out/pom.js
+++ wp/WPB2/scripts/out/pom.js
@@ -4,7 +4,7 @@ import DecServer from "./dec/server/DecServer.js";
 import ExGame from "./modules/exmc/server/ExGame.js";
 let config = new ExConfig();
 config.addonName = "POM";
-config.addonVersion = "1.7.2B2";
+config.addonVersion = "1.7.2B3";
 config.gameVersion = "1.9.50";
 config.watchDog = false;
 config.debug = true;
