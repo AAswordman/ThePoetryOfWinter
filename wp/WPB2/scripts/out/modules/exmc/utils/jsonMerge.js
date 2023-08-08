5fa794a6105bdd5aca8661c5462d3316859a3c08
AAswordman <66207760+AAswordman@users.noreply.github.com>
2023-08-08 08:21:19 +0800

    Delete wp/WPB2/scripts/out directory
    Former-commit-id: 1e4292b453612c6c4cbb8ecf222837261cdce121

--- wp/WPB2/scripts/out/modules/exmc/utils/jsonMerge.js
+++
@@ -1,9 +0,0 @@
-export default function jsonMerge(a, b) {
-    for (let i in b) {
-        if (!(i in a)) {
-            a[i] = b[i];
-        }
-    }
-    return a;
-}
-//# sourceMappingURL=jsonMerge.js.map
--- wp/WPB2/scripts/out/modules/exmc/utils/native/Array.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Array.prototype.clear = function () {
-    this.splice(0, this.length);
-};
-//# sourceMappingURL=Array.js.map
